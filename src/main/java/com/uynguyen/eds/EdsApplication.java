566031ab158dc2f96f5117f7303537d57b6eefbe
uy-nguyen00 <uynguyen1501@gmail.com>
2025-07-06 17:42:28 +0200

    base structure

---
+++ src/main/java/com/uynguyen/eds/EdsApplication.java
@@ -0,0 +1,13 @@
+package com.uynguyen.eds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uynguyen/eds/EdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uynguyen.eds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
